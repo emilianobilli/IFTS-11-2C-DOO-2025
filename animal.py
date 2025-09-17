2806c5be2daa69c0b76ba5292f95660fcb691f2a
Emiliano A. Billi <emiliano.billi@gmail.com>
2025-09-17 19:54:17 -0300

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,17 +1,38 @@
 from abc import ABC, abstractmethod
 
 class Animal(ABC):
-    @abstractmethod
-    def hablar(self):
-        pass
+    def __init__(self, name, posicion, velocidad):
+        self.name      = name 
+        self.posicion  = posicion
+        self.velocidad = velocidad
+
+    def caminar(self, tiempo):
+        self.posicion = self.velocidad * tiempo
 
 
 class Perro(Animal):
-    def hablar(self):
-        print("Wow Wow")
+    def __init__(self, name, posicion):
+        super().__init__(name, posicion, 2)
 
+    def caminar(self, tiempo):
+        print("Que camine tu hermana")
+
+class Dalmata(Perro):
+    def __init__(self, name, posicion):
+        super().__init__(name, posicion)
 
 class Gato(Animal):
-    def hablar(self):
-        print("Miau")
+    def __init__(self, name, posicion):
+        super().__init__(name, posicion, 1)
+
+class Diputado(Animal):
+    pass
+
+p = Perro("Victor", 0)
+g = Gato("Pedro", 0)
+
+p.caminar(10)
+g.caminar(10)
+
+print(p.posicion, g.posicion)
 
